3162d293cec849bff4fc92c61022b619451ae6b2
Ivgenium <genrymorgon@mail.ru>
2021-08-15 17:18:16 +0300

    6.0.2. Заполнение массива. [#226300]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,10 @@ public class ArrayDefinition {
         System.out.println("Размер массива surnames равен: " + surnames.length);
         float[] prices = new float[40];
         System.out.println("Размер массива prices равен: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Bertram Adams";
+        names[1] = "Margaret Thompson";
+        names[2] = "Hugh Gibson";
+        names[3] = "Magdalene Hopkins";
     }
 }
